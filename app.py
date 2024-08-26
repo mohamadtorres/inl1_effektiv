943b4e9ca6fe0fad7e5f13ba2cdea962239ba636
Mohamad Rahimi <m.rahimi.vasteras@gmail.com>
2024-08-26 15:13:25 +0200

    instagram

--- app.py
+++ app.py
@@ -5,61 +5,68 @@ from timeit import default_timer
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:golestan5@localhost/inl1'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:golestan5@localhost/inl1'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-faker = Faker('sv_SE')
+# db = SQLAlchemy(app)
+# faker = Faker('sv_SE')
 
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    namn = db.Column(db.String(100), nullable=False)
-    efternamn = db.Column(db.String(100), nullable=False)
-    personnummer = db.Column(db.String(12), unique=True, nullable=False)
-    stad = db.Column(db.String(100), nullable=False)
-    land = db.Column(db.String(100), nullable=False)
-    yrke = db.Column(db.String(100), nullable=False)
-    telefonnummer = db.Column(db.String(20), nullable=False)
+# class Person(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     namn = db.Column(db.String(100), nullable=False)
+#     efternamn = db.Column(db.String(100), nullable=False)
+#     personnummer = db.Column(db.String(12), unique=True, nullable=False)
+#     stad = db.Column(db.String(100), nullable=False)
+#     land = db.Column(db.String(100), nullable=False)
+#     yrke = db.Column(db.String(100), nullable=False)
+#     telefonnummer = db.Column(db.String(20), nullable=False)
 
-    def __repr__(self):
-        return f"<Person {self.namn} {self.efternamn}>"
+#     def __repr__(self):
+#         return f"<Person {self.namn} {self.efternamn}>"
 
 @app.route("/")
 def home():
     return render_template("base.html")
 
 
-def main():
-    t_start = default_timer()
-    total_records = 1000000
-    persons = []
+# def main():
+#     record_count = Person.query.count()
+#     if record_count > 0:
+#         print(f"Det finns {record_count} data i datasetet så INGA nya data")
+#         return
+    
 
-    for _ in range(total_records):
-        person = Person(
-            namn=faker.first_name(),
-            efternamn=faker.last_name(),
-            personnummer=faker.unique.ssn(),
-            stad=faker.city(),
-            land=faker.country(),
-            yrke=faker.job(),
-            telefonnummer=faker.phone_number()
-        )
-        persons.append(person)
+#     t_start = default_timer()
+#     total_records = 1000
+#     persons = []
 
-    try:
-        db.session.bulk_save_objects(persons)  
-        db.session.commit()
-        print("1,000,000 fake records have been added to the database!")
-    except Exception as e:
-        db.session.rollback()
-        print(f"An error occurred: {e}")
+#     for _ in range(total_records):
+#         person = Person(
+#             namn=faker.first_name(),
+#             efternamn=faker.last_name(),
+#             personnummer=faker.unique.ssn(),
+#             stad=faker.city(),
+#             land=faker.country(),
+#             yrke=faker.job(),
+#             telefonnummer=faker.phone_number()
+#         )
+#         persons.append(person)
+#         if _ % 100 == 0:  
+#             print(f"{_} records created...")
+#     try:
+#         db.session.bulk_save_objects(persons)  
+#         db.session.commit()
+#         print("1,000,000 fake records have been added to the database!")
+#     except Exception as e:
+#         db.session.rollback()
+#         print(f"An error occurred: {e}")
 
-    t_end = default_timer()
-    print(f"Det tog {t_end - t_start:.2f} sekunder att skapa listan!")
+#     t_end = default_timer()
+#     print(f"Det tog {t_end - t_start:.2f} sekunder att skapa listan!")
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all() 
-        main() 
+    # with app.app_context():
+    #     db.create_all() 
+    #     main() 
     app.run(debug=True)
